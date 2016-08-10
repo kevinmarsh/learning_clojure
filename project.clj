438ddd6905048b25aceb37fbe2d76af97242fdbd
Kevin Marsh <kevinmarsh3@gmail.com>
2016-08-10 12:40:25 +0100

    lein: set up Leiningen project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject learning_clojure "0.1.0-SNAPSHOT"
+  :description "Bunch of resources used for learning clojure"
+  :url "https://github.com/kevinmarsh/learning_clojure"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
